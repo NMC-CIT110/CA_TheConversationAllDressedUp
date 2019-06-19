a3e097d337b6c533cd149e75af56701da3a374b4
John Velis <john@johnvelis.com>
2019-06-19 15:33:46 -0400

    Initial Push

--- CA_TheConversation/Program.cs
+++ CA_TheConversation/Program.cs
@@ -11,7 +11,7 @@ namespace CA_TheConversationAllDressedUp
     // Application:     The Conversation All Dressed Up
     // Author:          Velis, John E
     // Description:     An application that has a conversation with the user
-    //                  about skiing. Console properties are controlled.
+    //                  about getting a loan. Console properties are controlled.
     // Date Created:    5/20/2019
     // Date Revised:    7/7/2019
     // *************************************************************
@@ -32,10 +32,12 @@ namespace CA_TheConversationAllDressedUp
             // variables
             //
             string userName;
-            string favoriteSki;
-            string typeOfSkiing;
-            string favoriteArea;
+            string bankChoice;
+            string typeOfLoan;
             string userResponse;
+            double principle;
+            double interestRate;
+            int numberOfYears;
 
 
             //
@@ -50,7 +52,7 @@ namespace CA_TheConversationAllDressedUp
             // display a welcome screen
             // 
             Console.WriteLine();
-            Console.WriteLine("\t\tThe Skiing Consultant");
+            Console.WriteLine("\t\tThe Loan Consultant");
             Console.WriteLine();
             Console.WriteLine("\tPress any key to continue.");
             Console.ReadKey();
@@ -67,24 +69,24 @@ namespace CA_TheConversationAllDressedUp
             // enter conversation with user
             //
             Console.WriteLine();
-            Console.WriteLine("Hello!");
-            Console.WriteLine("My name is John.");
+            Console.WriteLine(" Hello!");
+            Console.WriteLine(" This application is designed to calculate monthly loan payments.");
 
             //
             // get their name and echo it back
             //
             Console.WriteLine();
-            Console.Write("What is your name? ");
+            Console.Write(" What is your name? ");
             userName = Console.ReadLine();
-            Console.WriteLine("It is nice to meet you " + userName + ".");
+            Console.WriteLine(" It is nice to meet you " + userName + ".");
             //Console.WriteLine("It is nice to meet you {0}.", userName);
             //Console.WriteLine($"It is nice to meet you {userName}.");
 
             //
-            // determine if the user skis
+            // determine if the user is looking for a loan
             //
             Console.WriteLine();
-            Console.Write($"Do you like to ski {userName}?");
+            Console.Write($" Are you interested in getting a loan {userName}?");
             userResponse = Console.ReadLine();
 
             //
@@ -94,7 +96,7 @@ namespace CA_TheConversationAllDressedUp
             Console.Clear();
 
             //
-            // get more information if the user is a skier
+            // get more information if the user is looking for a loan
             //
             if (userResponse == "yes")
             {
@@ -102,36 +104,42 @@ namespace CA_TheConversationAllDressedUp
                 // get user's favorite ski
                 //
                 Console.WriteLine();
-                Console.WriteLine("Glad to hear you like skiing. It is one of my favorite sports!");
-                Console.Write("What is your favorite ski?");
-                favoriteSki = Console.ReadLine();
-                Console.WriteLine("So, {0}, you like {1} skis.", userName, favoriteSki);
+                Console.WriteLine(" Well you have come to the right place.");
+                Console.WriteLine();
+                Console.WriteLine(" I will need to get some more information from you and then I can calculate your monthly payment.");
 
                 //
-                // get the user's type of skiing
+                // *** New Screen ***
+                // clear screen
                 //
-                Console.WriteLine();
-                Console.WriteLine("I am interested in what kind of skiing you do.");
-                Console.Write("Do you prefer \"downhill\" or \"back country\"?");
-                typeOfSkiing = Console.ReadLine();
+                Console.Clear();
 
                 //
-                // get the user's favorite area
+                // get user ski info
                 //
                 Console.WriteLine();
-                Console.Write("What is your favorite area to ski.");
-                favoriteArea = Console.ReadLine();
+                Console.Write(" What is the name of the bank you will applying to for the loan?");
+                bankChoice = Console.ReadLine();
+                Console.WriteLine(" Very good {0}, you will be getting a loan from {1}.", userName, bankChoice);
+
+                //
+                // get the user's type of loan
+                //
+                Console.WriteLine();
+                Console.WriteLine(" Please tell me the type of loan your are seeking so that I can determine the interest rate.");
+                Console.Write("  \"House\"  \"Vehicle\"  \"College\"  \"Personal\"");
+                typeOfLoan = Console.ReadLine();
 
                 //
                 // provide feedback on the type of skiing
                 //
                 Console.WriteLine();
-                if (typeOfSkiing == "downhill")
+                if (typeOfLoan == "downhill")
                 {
                     Console.WriteLine("I have downhill skied for most of my life, but gave it up last year.");
                     Console.WriteLine($"I hear the {favoriteArea} is a great place to ski {userName}.");
                 }
-                else if (typeOfSkiing == "back country")
+                else if (typeOfLoan == "back country")
                 {
                     Console.WriteLine("I spend most of my time skiing now in back country.");
                     Console.WriteLine($"Well {userName}, I am not sure I have skied at {favoriteArea}. I will have to give it a try.");
@@ -148,19 +156,19 @@ namespace CA_TheConversationAllDressedUp
                 Console.WriteLine($"{userName}, it has been great getting learn more about your skiing interests.");
                 Console.WriteLine("Have a nice day.");
             }
-            else // user does not like to ski
+            else // user does not want a loan
             {
                 Console.WriteLine();
-                Console.WriteLine($"I am sorry you don't like skiing {userName}. It has been nice meeting you.");
-                Console.WriteLine($"This application is designed for skiers. It has been nice meeting you.");
-                Console.WriteLine("Have a nice day.");
+                Console.WriteLine($" I am sorry you are not looking for a loan {userName}, but this application ");
+                Console.WriteLine(" is for people interested in the monthly payments for a specific loan.");
+                Console.WriteLine(" Have a nice day.");
             }
 
             //
             // display a prompt to exit the application
             //
             Console.WriteLine();
-            Console.WriteLine("Press any key to continue.");
+            Console.WriteLine(" Press any key to continue.");
             Console.ReadKey();
 
             //
